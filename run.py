70ad585f00566b0ba6aad031db82946a73f67bbc
gabry1242 <gabriele.gianuz@gmail.com>
2026-03-02 00:35:43 +0100

    keywords detection

--- run.py
+++ run.py
@@ -1,17 +1,56 @@
 import re
 import spacy
+from spacy.lang.en import English
 from spacy.matcher import PhraseMatcher, Matcher
+import pandas as pd
 
-nlp = spacy.load("en_core_web_sm")
 
-phrase_keywords = [
-    "battery life",
-    "customer service",
-    "refund policy",
-]
+#sentence-transformers/all-MiniLM-L6-v2 
+#Possibly look into this to have also different phrase embeddings
 
-phrase_matcher = PhraseMatcher(nlp.vocab, attr="LOWER")
-phrase_patterns = [nlp.make_doc(p) for p in phrase_keywords]
-phrase_matcher.add("KW_PHRASE", phrase_patterns)
+df = pd.read_csv("processed_dataset.csv")
+df_list = list(df["string"])
 
-print(phrase_patterns)
+nlp = English()
+
+#distinguish between words and phrases
+single = [k for k in df_list if " " not in k]
+multi  = [k for k in df_list if " " in k]
+
+#phrase case exact finding
+matcher_phrase = PhraseMatcher(nlp.vocab, attr="LOWER")
+multi_patterns = [nlp.make_doc(p) for p in multi if p]
+if multi_patterns:
+    matcher_phrase.add("KW_PHRASE", multi_patterns)
+
+#word case
+token_matcher = Matcher(nlp.vocab)
+
+#done to match word that can be similar
+#e.g word.lower convert the word in lowercase, re.escape escapes special characters so su+re becomes su\+\re
+# rf is need to raw format, raw removes the backslash and format allows to use {}
+# ^ means that the words starts 
+# \w* means zero or more characters
+# $ means end of sequence 
+def make_regex(word: str) -> str:
+    return rf"^{re.escape(word.lower())}\w*$"
+
+
+for w in single:
+    token_matcher.add(f"KW_{w}", [[{"LOWER": {"REGEX": make_regex(w)}}]])
+
+
+def find_keywords(text: str):
+    doc = nlp(text)
+    hits = []
+
+    for _, start, end in matcher_phrase(doc):
+        hits.append(doc[start:end].text)
+
+    for match_id, start, end in token_matcher(doc):
+        hits.append(doc[start:end].text)
+
+    return (1 if hits else 0), sorted(set(hits))
+
+flag, hits = find_keywords("Brother I am really IshowSPEEDING if you know what I mean.")
+print(flag, hits)
